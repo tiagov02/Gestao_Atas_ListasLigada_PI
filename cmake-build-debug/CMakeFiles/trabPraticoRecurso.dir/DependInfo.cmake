1230c98a4c8e38dbeda9e74542441623170913bd
tiagov02 <96255633+tiagov02@users.noreply.github.com>
2022-07-18 17:57:31 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tiagoviana/Desktop/trabPraticoRecurso/gestao_atas.c" "/Users/tiagoviana/Desktop/trabPraticoRecurso/cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/gestao_atas.c.o"
+  "/Users/tiagoviana/Desktop/trabPraticoRecurso/loginsGestaoUsers.c" "/Users/tiagoviana/Desktop/trabPraticoRecurso/cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/loginsGestaoUsers.c.o"
+  "/Users/tiagoviana/Desktop/trabPraticoRecurso/main.c" "/Users/tiagoviana/Desktop/trabPraticoRecurso/cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/main.c.o"
+  "/Users/tiagoviana/Desktop/trabPraticoRecurso/menus.c" "/Users/tiagoviana/Desktop/trabPraticoRecurso/cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/menus.c.o"
+  "/Users/tiagoviana/Desktop/trabPraticoRecurso/stack.c" "/Users/tiagoviana/Desktop/trabPraticoRecurso/cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/stack.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/trabPraticoRecurso.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabPraticoRecurso.dir/gestao_atas.c.o"
+  "CMakeFiles/trabPraticoRecurso.dir/loginsGestaoUsers.c.o"
+  "CMakeFiles/trabPraticoRecurso.dir/main.c.o"
+  "CMakeFiles/trabPraticoRecurso.dir/menus.c.o"
+  "CMakeFiles/trabPraticoRecurso.dir/stack.c.o"
+  "trabPraticoRecurso"
+  "trabPraticoRecurso.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trabPraticoRecurso.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
